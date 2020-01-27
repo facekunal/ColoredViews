fca2d9db84d15559918b6f5b8afd46f97765b262
facekunal <facekunal@gmail.com>
2020-01-28 02:26:11 +0530

    Add bottom contol buttons

--- app/src/main/java/com/example/coloredviews/MainActivity.kt
+++ app/src/main/java/com/example/coloredviews/MainActivity.kt
@@ -15,7 +15,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setListeners() {
-        val viewList: List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+        val viewList: List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text,
+            box_five_text, constraint_layout, red_button, green_button, yellow_button)
         for(item in viewList) {
             item.setOnClickListener(this::changeColor)
         }
@@ -25,9 +26,12 @@ class MainActivity : AppCompatActivity() {
         when(view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.CYAN)
             R.id.box_two_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_red_dark)
-            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_light)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.red)
+            R.id.green_button -> box_four_text.setBackgroundResource(R.color.green)
+            R.id.yellow_button -> box_five_text.setBackgroundResource(R.color.yellow)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
